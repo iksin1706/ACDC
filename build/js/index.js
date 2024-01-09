e2f8c58c0502c36a0b24fe4f9b46ddfecdc7d487
iksin1706 <49130419+iksin1706@users.noreply.github.com>
2024-01-09 23:28:45 +0100

    optimalization

--- build/js/index.js
+++ build/js/index.js
@@ -1,18 +1,18 @@
-tailwind.config = {
-  theme: {
-    extend: {
-      animation: {
-        'infinite-scroll': 'infinite-scroll 25s linear infinite',
-      },
-      keyframes: {
-        'infinite-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
-      }
-    },
-  },
-}
+// tailwind.config = {
+//   theme: {
+//     extend: {
+//       animation: {
+//         'infinite-scroll': 'infinite-scroll 25s linear infinite',
+//       },
+//       keyframes: {
+//         'infinite-scroll': {
+//           from: { transform: 'translateX(0)' },
+//           to: { transform: 'translateX(-100%)' },
+//         }
+//       }
+//     },
+//   },
+// }
 
 const sliderLabels = [
   { first: "Instalacje", second: "elektryczne" },
